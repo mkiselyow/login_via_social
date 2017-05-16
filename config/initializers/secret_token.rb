99f7debf46a4125141f450f90ac972d47c8e963e
MaximKiselyow <m.kiselyow@gmail.com>
2017-05-16 16:51:03 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LightIt::Application.config.secret_token = '3ecfc30a2b0adb31e744adeacbabe049d2ce4d5e65b58483f23180e000150c4d6440b8b49cd20fe6b7520e91c96072fbc1637f6fd9efa921fc142c4f79399b80'
