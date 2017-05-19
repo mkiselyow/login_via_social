90425f6282d28f0358e7e159ffdf7cec485cfd3b
MaximKiselyow <m.kiselyow@gmail.com>
2017-05-19 05:37:31 +0300

    vk_login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1973951366220686', '640de0f44e8d9ec79af4eccf6661b10c', scope: 'email', info_fields: 'email,name,first_name,last_name,gender'
-  provider :vkontakte, '6036834', 'MUA7TxNQ43Mr4HboAshV', scope: 'email', info_fields: 'email,name,first_name,last_name,gender', :display => 'popup',
+  provider :vkontakte, '6036834', 'MUA7TxNQ43Mr4HboAshV', scope: 'friends,audio,photos,email', info_fields: 'email,name,first_name,last_name,gender', :display => 'popup'
 end
