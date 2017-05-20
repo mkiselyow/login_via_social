3f87ab5641c7dd66dbbf94416647d24fff5e21f5
MaximKiselyow <m.kiselyow@gmail.com>
2017-05-20 05:44:05 +0300

    posts_improved

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,7 @@ class StaticPagesController < ApplicationController
   def index 
     if signed_in?
       @post  = current_user.posts.build
-      @feed_items = current_user.feed
     else
-      @post  = Post.all
       @feed_items = Post.all
     end
   end
